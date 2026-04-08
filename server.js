78c25f3e5f095f2c1073bff46b24df225f3328b5
myracleai <myracleai@gmail.com>
2026-04-08 08:56:42 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -472,13 +472,21 @@ app.listen(PORT, function() {
   var u = req.query.u || 'nike';
   var logs = [];
 
-  // Test publish.twitter.com via proxy
-  var r1 = await fetchProxy('https://publish.twitter.com/oembed?url=https://twitter.com/' + u);
-  logs.push({ p:'oembed_proxy', status:r1.status, len:r1.body.length, body:r1.body.substring(0,200) });
+  // Test 1: LinkedIn via proxy
+  var r1 = await fetchProxy('https://www.linkedin.com/in/' + u + '/');
+  logs.push({ p:'linkedin_proxy', status:r1.status, len:r1.body.length, snippet:r1.body.substring(0,300) });
 
-  // Test publish.twitter.com direct
-  var r2 = await fetchDirect('https://publish.twitter.com/oembed?url=https://twitter.com/' + u);
-  logs.push({ p:'oembed_direct', status:r2.status, len:r2.body.length, body:r2.body.substring(0,200) });
+  // Test 2: LinkedIn direct
+  var r2 = await fetchDirect('https://www.linkedin.com/in/' + u + '/');
+  logs.push({ p:'linkedin_direct', status:r2.status, len:r2.body.length, snippet:r2.body.substring(0,300) });
+
+  // Test 3: LinkedIn public profile API
+  var r3 = await fetchProxy('https://www.linkedin.com/in/' + u);
+  logs.push({ p:'linkedin_no_slash', status:r3.status, len:r3.body.length, snippet:r3.body.substring(0,300) });
+
+  // Test 4: LinkedIn public posts
+  var r4 = await fetchProxy('https://www.linkedin.com/posts/' + u);
+  logs.push({ p:'linkedin_posts', status:r4.status, len:r4.body.length, snippet:r4.body.substring(0,200) });
 
   res.json({ username: u, results: logs });
 });
